8b03b65b3cd34c5161fc8b32784b8afc27e6ead5
R503B <sodanak1n@outlook.com>
2021-02-19 09:22:04 +0100

    Added nodes for nodes shown on Screenshot.

--- WolvenKit/WKitGlobal/NodeNetworkHelper.cs
+++ WolvenKit/WKitGlobal/NodeNetworkHelper.cs
@@ -1,4 +1,4 @@
-﻿using DynamicData;
+using DynamicData;
 using NodeNetwork.Toolkit.ValueNode;
 using NodeNetwork.ViewModels;
 using ReactiveUI;
@@ -15,17 +15,94 @@ namespace WolvenKit.WKitGlobal
 {
     class NodeNetworkHelper
     {
-        public enum NodeType
+        public enum ERXNodes
         {
-            StartNode, SceneNode, PhaseNode, InputNode,
-            OutputNode, DeletionNode, PauseNode, InteractiveObjectManagerNode,
-            DeviceManagerNode, ConditionNode, QuestNode, HubNode,
-            EndNode, ChoiceNode, SectionNode, RandomizerNode,
-            XorNode, FactsDBManagerNode, LogicalHubNode, WorldDataManagerNode,
-            AudioNode, TeleportPuppetNode, UIManagerNode, RenderFXNode,
-            LogicalXorNode, RewardManagerNode, CheckpointNode, CharacterManagerNode,
-            PhoneManagerNode, CutControlNode,
+            
+     
+           
+            Deletion, // Placeholder for patching (Passes a signal if the player had a savegame at this place in the quest) 
+         
+            Section,
+            Choise,
+            Scene,
+            EquipItem,         //EquipItem can equip or unequip items
+            
+
+            StartMinigame,
+            TransformAnimator,
+            Move,
+            VehicleMove,
+            Teleport,
+            Save,
+
+            Input, // IN_NODE
+            Output, // OUT_NODE
+            Switch, // Switch_Node
+            Randomizer,
+            CutControl,
+            Phase,
+            AND,
+            XOR, // X 
+            FlowControl,
+            Hub,
+            PauseCondition,
+            Condition,
+
+            Comment,
+            Note,
+
+            EventManager,
+            JournalManager,
+            AudioManager,
+            CharacterAudioManager,
+            WorkspotManager,  // 
+            FXManager,
+            SceneManager,
+            GameManager,
+            UIManager,
+            AIManager, // AI manager node itself switches NPCs between Cinematic and Gameplay AI
+            WorldDataManager,
+            EntityManager,
+            InteractiveObjectManager,
+            RenderManager,
+            TimeManager,
+            ItemManager, //ItemManager can do all sorts of shit
+            PhoneManager,
+            SpawnManager,
+            FactsManager,
+            InterruptionManager,
+            LookAtManager,
+            CrowdManager,
+            RewardManager,
+            VoiceSetManager,
+            VisionSystemManager,
+            MapPinManager,
+            CharacterManager,
+            CombatManager,
+            VehicleManager,
+            AICommandsManager, //Immediate : with it we can set npcs to patrol, be on proper Follower AI or clear that follower ai
+
+
+
+
+
+
         }
+
+        public enum NodeGroup
+        {
+            BehaviorControl,
+            Camera,
+            ComplexityManagement,
+            FlowControl,
+            GameSystemsControl,
+            Gameplay,
+            Logical,
+            QuestLog,
+            Scenes,
+            Scripting
+        }
+     
 
 
         public class Node_Template
@@ -41,37 +118,37 @@ namespace WolvenKit.WKitGlobal
 
     public class CustomNode : NodeViewModel
     {
-        //public Color nodecolor;
+        public Color nodecolor;
 
 
-        //public CustomNode(
-        //    string NodeName,
-        //    Color NodeColor,
-        //    string NodeDescription,
-        //    string NodeFamily,
+        public CustomNode(
+            string NodeName,
+            Color NodeColor,
+            string NodeDescription,
+            string NodeFamily,
 
 
 
-        //    List<ValueNodeInputViewModel<Type>> InputSockets,
-        //    List<ValueNodeOutputViewModel<Type>> OutputSockets)
-        //{
-        //    this.Name = NodeName;
-        //    nodecolor = NodeColor;
+            List<ValueNodeInputViewModel<Type>> InputSockets,
+            List<ValueNodeOutputViewModel<Type>> OutputSockets)
+        {
+            this.Name = NodeName;
+            nodecolor = NodeColor;
 
 
-        //    foreach (var InSocket in InputSockets)
-        //    {
-        //        this.Inputs.Add(InSocket);
-        //    }
+            foreach (var InSocket in InputSockets)
+            {
+                this.Inputs.Add(InSocket);
+            }
 
 
-        //    foreach (var OutSocket in OutputSockets)
-        //    {
-        //        this.Outputs.Add(OutSocket);
-        //    }
+            foreach (var OutSocket in OutputSockets)
+            {
+                this.Outputs.Add(OutSocket);
+            }
 
 
-        //}
+        }
     }
 
 
